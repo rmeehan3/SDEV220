5e4589097f5eff83f0748bd1a68a0c991c5c1f12
rmeehan3 <136287809+rmeehan3@users.noreply.github.com>
2023-06-24 23:28:13 -0400

    Add m03 lab
    .py file for my m03 lab program

---
+++ m03_lab.py
@@ -0,0 +1,43 @@
+# Ryder Meehan
+# m03_lab.py
+# This program utilizes classes and the super() class function
+# to ask the user for vehicle information and store each attribute
+# into the Automobile class and then output the vehicle information
+# back to the user in an organized format
+
+class Vehicle:
+    def __init__(self, type):
+        self.type = type
+
+class Automobile(Vehicle):
+    def __init__(self, type, year, make, model, doors, roof):
+        super().__init__(type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+type = input("Enter type of vehicle: ")
+
+year = input("Year: ")
+
+make = input("Make: ")
+
+model = input("Model: ")
+
+doors = input("Number of doors: ")
+
+roof = input("Roof type: ")
+
+whip = Automobile(type, year, make, model, doors, roof)
+
+print("\nVehicle Info\n_____________\n")
+print("Vehicle Type:", whip.type)
+print("Year:", whip.year)
+print("Make:", whip.make)
+print("Model:", whip.model)
+print("Doors:", whip.doors)
+print("Roof Type:", whip.roof)
+
+input("Press ENTER to exit") #put this here to stop the program for immediately closing once output was complete
